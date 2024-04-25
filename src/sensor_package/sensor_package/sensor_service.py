78332cf080be8699ba3654b4ae4d0b5208f8ca2a
varunthepianoman <varunkamat99@gmail.com>
2024-04-24 18:57:16 -0700

    signing off for wed apr 24 - next step is importing official sensor.py

--- src/sensor_package/sensor_package/sensor_service.py
+++ src/sensor_package/sensor_package/sensor_service.py
@@ -4,15 +4,37 @@ from custom_interfaces.msg import SensorSample
 import rclpy
 from rclpy.node import Node
 import numpy as np
+from sensor import Sensor
+import socket
+import sys
+import time
+
 
 class SensorService(Node):
 
     def __init__(self):
         super().__init__('sensor_service')
         self.srv = self.create_service(SensorRead, 'sensor_read_service', self.sensor_read_callback)
+        self.sensor = Sensor('127.0.0.1', 65432, 2000, 0.001) # Define a sensor with 2000Hz sampling rate and 1ms delay
+        
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = ('127.0.0.1', 65432)
+        print('connecting to {} port {}'.format(*server_address))
+        sock.connect(server_address)
 
     def sensor_read_callback(self, request, response):
-        sensor_data = np.random.rand(6, request.num_samples)
+        # Request num_samples samples from the sensor
+        message_string = str(request.num_samples)
+        message = message_string.encode()
+        sock.sendall(message)
+
+        byte_data = sock.recv(10000)
+        sensor_data = np.frombuffer(byte_data) 
+
+
         Sensor_Samples = []
         for i in range(request.num_samples):
             datapoint = SensorSample()
