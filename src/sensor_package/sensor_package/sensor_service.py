7ddb6731ded2cf6d4a34e2c7f353af743e75e8d6
varunthepianoman <varunkamat99@gmail.com>
2024-04-25 14:01:15 -0700

    fixes to sensor in service

--- src/sensor_package/sensor_package/sensor_service.py
+++ src/sensor_package/sensor_package/sensor_service.py
@@ -4,11 +4,11 @@ from custom_interfaces.msg import SensorSample
 import rclpy
 from rclpy.node import Node
 import numpy as np
-from sensor import Sensor
+from .submodules.sensor import Sensor
 import socket
 import sys
 import time
-
+from threading import Thread, Timer
 
 class SensorService(Node):
 
@@ -16,24 +16,31 @@ class SensorService(Node):
         super().__init__('sensor_service')
         self.srv = self.create_service(SensorRead, 'sensor_read_service', self.sensor_read_callback)
         self.sensor = Sensor('127.0.0.1', 65432, 2000, 0.001) # Define a sensor with 2000Hz sampling rate and 1ms delay
-        
+        t1 = Thread(target = self.sensor.run)
+        t1.daemon = True
+        t1.start()
+
         # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Connect the socket to the port where the server is listening
         server_address = ('127.0.0.1', 65432)
         print('connecting to {} port {}'.format(*server_address))
-        sock.connect(server_address)
+        self.sock.connect(server_address)
+        print('connected')
 
     def sensor_read_callback(self, request, response):
         # Request num_samples samples from the sensor
+        print('callback from sensor')
         message_string = str(request.num_samples)
         message = message_string.encode()
-        sock.sendall(message)
-
-        byte_data = sock.recv(10000)
-        sensor_data = np.frombuffer(byte_data) 
-
+        print('beforesendall')
+        self.sock.sendall(message)
+        print('aftersendall')
+        byte_data = self.sock.recv(10000)
+        print('aftersock.recv')
+        sensor_data = np.frombuffer(byte_data).reshape(self.sensor.DOF, request.num_samples)
+        print('sensor_data', sensor_data)
 
         Sensor_Samples = []
         for i in range(request.num_samples):
