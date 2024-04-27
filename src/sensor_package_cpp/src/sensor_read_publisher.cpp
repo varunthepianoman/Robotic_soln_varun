22ac5cf1968c1d5ba443f0131a9527d56dd00261
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-04-26 20:29:48 -0700

    Friday Apr 26 Checkpoint: Draft of Code, Not Running Yet

--- src/sensor_package_cpp/src/sensor_read_publisher.cpp
+++ src/sensor_package_cpp/src/sensor_read_publisher.cpp
@@ -11,9 +11,6 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
 class SensorReadPublisher : public rclcpp::Node
 {
 public:
