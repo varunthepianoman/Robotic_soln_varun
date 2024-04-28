af837305d4a62e19e75287fd892c864cd18fd067
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-04-28 12:22:32 -0700

    client qos

--- src/sensor_package_cpp/src/sensor_client.cpp
+++ src/sensor_package_cpp/src/sensor_client.cpp
@@ -21,7 +21,7 @@ public:
             // The clients are called within the publisher, so they must be in different Callback Groups than Publisher to avoid deadlock.
             // Clients 1 and 2 can be called in parallel, so place them in different Callback Groups as well.
             // Favoring MutuallyExclusive over Reentrant as it is safer: We then won't have two queries accessing same server data_reservoir.
-            qos_profile = rclcp
+//            qos_profile = rclcp
 
             rclcpp::CallbackGroup::SharedPtr callback_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
             rclcpp::Client<custom_interfaces::srv::SensorRead>::SharedPtr client = this->create_client<custom_interfaces::srv::SensorRead>("sensor1_read_service", rmw_qos_profile_system_default, callback_group);
