9f281eaee4d4aa4a76a4aa626e0f7a8b27ca619b
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-04-28 16:28:53 -0700

    publisher compliing

--- src/sensor_package_cpp/src/sensor_client.cpp
+++ src/sensor_package_cpp/src/sensor_client.cpp
@@ -51,7 +51,8 @@ public:
             RCLCPP_INFO(this->get_logger(), "Received response");
         }
 
-        // Make static to preserve this returned variable NOTE deleting get
+        auto readings = result_future.readings
+        // Make static to preserve this returned variable
         static rclcpp::Client<custom_interfaces::srv::SensorRead>::SharedResponse result_future_get = result_future.get();
 
         return &result_future_get;
