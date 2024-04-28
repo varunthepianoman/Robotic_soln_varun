e2a5baefd4e3a26f37b0c03592511e9017f738bf
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-04-28 10:10:02 -0700

    debugging to get sensor_client to compile

--- src/sensor_package_cpp/src/sensor_client.cpp
+++ src/sensor_package_cpp/src/sensor_client.cpp
@@ -20,7 +20,7 @@ public:
         this->sensor_client = sensor_client;
     }
 
-    &custom_interfaces::srv::SensorRead::Response send_request(int num_samples):
+    &(custom_interfaces::srv::SensorRead::Response) send_request(int num_samples):
     {
         auto request = std::make_shared<custom_interfaces::srv::SensorRead::Request>();
         request->num_samples = num_samples
