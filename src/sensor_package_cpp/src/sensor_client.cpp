2d207a87a2296bb71be1f429e802cd5ac7b85ac8
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-04-29 12:29:59 -0700

    publisher compliing

--- src/sensor_package_cpp/src/sensor_client.cpp
+++ src/sensor_package_cpp/src/sensor_client.cpp
@@ -32,7 +32,7 @@ public:
     auto send_request()
     {
         rclcpp::Client<custom_interfaces::srv::SensorRead>::SharedRequest request;
-        auto request = std::make_shared<custom_interfaces::srv::SensorRead::Request>();
+        request = std::make_shared<custom_interfaces::srv::SensorRead::Request>();
         request->num_samples = this->num_samples;
 
         while (!sensor_client->wait_for_service(1s)) {
@@ -43,7 +43,8 @@ public:
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
         }
 
-        rclcpp::Client<custom_interfaces::srv::SensorRead>::FutureAndRequestId result_future = sensor_client->async_send_request(request);
+        // rclcpp::Client<custom_interfaces::srv::SensorRead>::FutureAndRequestId
+        auto result_future = sensor_client->async_send_request(request);
 
         // Must wait for result as we have allowed Publisher and Client to run concurrently (in order to avoid deadlock). Timeout to guarantee a graceful finish
         std::future_status status = result_future.wait_for(1s);
@@ -52,7 +53,8 @@ public:
             RCLCPP_INFO(this->get_logger(), "Received response");
         }
 
-        const rclcpp::Client<custom_interfaces::srv::SensorRead>::SharedResponse result_future_get = result_future.get();
+        // const rclcpp::Client<custom_interfaces::srv::SensorRead>::SharedResponse
+        auto result_future_shared = result_future.future.share();
 
         // Curiously, the SharedResponse object seems to have overloaded the address-of (&) operator. I tried to return a pointer to the SharedResponse object and extract readings in the Publisher, but it gave me and allocator instead of an address.
         // So, I just extract the readings here and return a pointer to them.
