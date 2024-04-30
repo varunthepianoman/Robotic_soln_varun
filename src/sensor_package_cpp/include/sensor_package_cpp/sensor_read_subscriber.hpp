fbfa5bbc8e06c08051048bb14c377e854e55645f
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-04-29 20:17:49 -0700

    Subscriber Compiling

--- src/sensor_package_cpp/include/sensor_package_cpp/sensor_read_subscriber.hpp
+++ src/sensor_package_cpp/include/sensor_package_cpp/sensor_read_subscriber.hpp
@@ -2,6 +2,10 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "custom_interfaces/msg/sensor_read_combined.hpp"
+#include "std_msgs/msg/string.hpp"
+#include<cstring>
+#include<string>
+
 #include <iostream>
 using std::placeholders::_1;
 
@@ -15,18 +19,40 @@ public:
         rclcpp::SubscriptionOptions options;
         options.callback_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
 
-        // subscription_ = this->create_subscription<custom_interfaces::msg::SensorReadCombined>(
-        //         "sensor_read_500hz", 10, std::bind(&SensorReadSubscriber::topic_callback, this, _1), options);
+        subscription_ = this->create_subscription<custom_interfaces::msg::SensorReadCombined>(
+                 "sensor_read_500hz", 10, std::bind(&SensorReadSubscriber::topic_callback, this, _1), options);
 
     }
 
 private:
-    void topic_callback(const std_msgs::msg::String & msg) const
+    rclcpp::Subscription<custom_interfaces::msg::SensorReadCombined>::SharedPtr subscription_;
+    void topic_callback(const custom_interfaces::msg::SensorReadCombined & msg) const
     {
         RCLCPP_INFO(this->get_logger(), "Heard");
-//        std::cout << msg
+        std::shared_ptr<const rosidl_runtime_cpp::BoundedVector<custom_interfaces::msg::SensorSample, 2>> sensor1_data = std::make_shared<const rosidl_runtime_cpp::BoundedVector<custom_interfaces::msg::SensorSample, 2>>(msg.readings_sensor1);
+        std::shared_ptr<const rosidl_runtime_cpp::BoundedVector<custom_interfaces::msg::SensorSample, 8>> sensor2_data = std::make_shared<const rosidl_runtime_cpp::BoundedVector<custom_interfaces::msg::SensorSample, 8>>(msg.readings_sensor2);
+        RCLCPP_INFO(this->get_logger(), "Sensor 1 Data:\n");
+        print_sensor_sample<2>(sensor1_data);
+        RCLCPP_INFO(this->get_logger(), "Sensor 2 Data:\n");
+        print_sensor_sample<8>(sensor2_data);
     }
-    rclcpp::Subscription<custom_interfaces::msg::SensorReadCombined>::SharedPtr subscription_;
+
+    template<int N>
+    void print_sensor_sample(const std::shared_ptr<rosidl_runtime_cpp::BoundedVector<custom_interfaces::msg::SensorSample, N>> sensor_data) const
+    {
+        int i = 0;
+        for (custom_interfaces::msg::SensorSample sample : *sensor_data) {
+            std::string sample_line = "Sample " + std::to_string(i) + ": ";
+            RCLCPP_INFO(this->get_logger(), sample_line.c_str());
+            std::string data_line = "[";
+            for (double datapoint : sample.data) {
+                data_line += std::to_string(datapoint) + " ";
+            }
+            data_line += "]";
+            RCLCPP_INFO(this->get_logger(), data_line.c_str());
+            i++;
+        }
+    }
 };
 
 //int main(int argc, char * argv[])
