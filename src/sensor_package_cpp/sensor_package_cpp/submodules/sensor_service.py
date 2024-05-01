c64a7f85063cc796e8687b7b64748fb732f1caf4
varunthepianoman <varunkamat99@gmail.com>
2024-05-01 11:31:47 -0700

    no sock

--- src/sensor_package_cpp/sensor_package_cpp/submodules/sensor_service.py
+++ src/sensor_package_cpp/sensor_package_cpp/submodules/sensor_service.py
@@ -78,6 +78,7 @@ class SensorService(Node):
         while True:
             message_string = str(self.number_of_samples)
             message = message_string.encode()
+            breakpoint()
             self.sock.sendall(message)
 
             byte_data = self.sock.recv(10000)
